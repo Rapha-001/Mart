ba55d43e0b2f5fea5d0257d7150fa923bece13f7
Raphael <akpabiorapha001@gmail.com>
2026-02-22 08:13:09 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "unimart-v12";
+
+const assetsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./app.js",
+  "./firebase.js",
+  "./cloudinary.js",
+  "./marketplace.js",
+  "./auth.js",
+  "./ui.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first, cache fallback
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
