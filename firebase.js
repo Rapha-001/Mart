2a6fb0bcf4a387eb4e0ac2413a15cd6359064c6e
Raphael <akpabiorapha001@gmail.com>
2026-05-13 11:57:48 +0100

    Remove Firebase messaging setup
    Removed unused Firebase messaging import and initialization.

--- firebase.js
+++ firebase.js
@@ -54,13 +54,6 @@ document.dispatchEvent(new Event('appReady'));
   }
 })();
 
-import { getMessaging } from "firebase/messaging";
-
-// ... assuming 'app' has already been initialized somewhere else
-
-const messaging = getMessaging(app);
-
-
 function showMaintenancePage(message) {
   // Hide everything and show maintenance screen
   document.querySelectorAll('body > *:not(#maintenanceScreen)').forEach(el => {
